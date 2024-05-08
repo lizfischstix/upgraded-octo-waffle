a5c810a867513b6f7a11771cdca5d001fc229dde
Liz FIschenich <Liz.Devlin85@gmail.com>
2024-05-08 11:30:56 -0500

    vite initialized, express/mysql up and runing. back and front connected. TODO ui/ux development

---
+++ server/models/index.js
@@ -0,0 +1,25 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+//connects user to posts
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+})
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+})
+module.exports = { User, Post, Comment };
---
+++ server/server.js
@@ -0,0 +1,89 @@
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// // Set up Handlebars.js engine with custom helpers
+// const hbs = exphbs.create({ helpers });
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 21600000,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+
+// // Inform Express.js on which template engine to use
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers/index'); // Import the combined routes file
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 21600000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/', routes); 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/index.html'));
+});
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(5000, () => console.log('Now listening on port 5000'));
+});
